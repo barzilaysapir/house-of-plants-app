46eb270da90aace4fc763cad1452b69af6d832e7
barzilaysapir <barzilaysapir@gmail.com>
2023-09-23 20:55:20 +0300

    api calls

--- server/dist/shared/species.js
+++ server/dist/shared/species.js
@@ -1,12 +1,17 @@
 "use strict";
-// quicktype your-sample.json --lang csharp -o output.cs
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Status = exports.Rank = void 0;
 var Rank;
 (function (Rank) {
     Rank["Species"] = "species";
+    Rank["Subspecies"] = "ssp";
+    Rank["Variety"] = "var";
+    Rank["Form"] = "form";
+    Rank["Hybrid"] = "hybrid";
+    Rank["Subvariety"] = "subvar";
 })(Rank || (exports.Rank = Rank = {}));
 var Status;
 (function (Status) {
     Status["Accepted"] = "accepted";
+    Status["Unknown"] = "unknown";
 })(Status || (exports.Status = Status = {}));
