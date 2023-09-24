f6cb50f5301205766aa0aa69ff9590f9a78db903
barzilaysapir <barzilaysapir@gmail.com>
2023-09-24 11:18:14 +0300

    change server dir

--- server/dist/shared/consts.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BASE_URL = exports.TREFLE_TOKEN = exports.PORT = void 0;
-exports.PORT = 4000;
-exports.TREFLE_TOKEN = "0rb0D_osH3J27ZVWN8CWNrpv1o-2zKP_aPfXtnq0mgY";
-exports.BASE_URL = "https://trefle.io/api/v1";
