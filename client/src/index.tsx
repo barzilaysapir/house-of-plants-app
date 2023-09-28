8604c3dfca23f61d5612012d3ceffb29d213d28c
moshe ben baruch <moshebe@elad.co.il>
2023-09-28 19:05:24 +0300

    pwa scaffolding

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import LoaderBackdrop from "components/LoaderBackdrop/LoaderBackdrop";
 import { router } from "config/router";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import "config/locales/i18n";
 
 const root = ReactDOM.createRoot(
@@ -15,6 +16,11 @@ root.render(
         <RouterProvider router={router} fallbackElement={<LoaderBackdrop />} />
     </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
