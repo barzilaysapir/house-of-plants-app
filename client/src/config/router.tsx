5550dfb15f77f9806274ab21654268393d193d39
Sapir Barzilay <sapirb@elad.co.il>
2023-09-30 19:16:28 +0300

    remove mock loader

--- client/src/config/router.tsx
+++ client/src/config/router.tsx
@@ -4,14 +4,12 @@ import Identify from "pages/Identify/Identify";
 import MyPlants from "pages/MyPlants/MyPlants";
 import { myPlantsLoader } from "pages/MyPlants/MyPlants.util";
 import { createBrowserRouter } from "react-router-dom";
-import { appLoader } from "./appLoader";
 import SignIn from "pages/SignIn/SignIn";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        loader: appLoader,
         children: [
             {
                 path: "",
