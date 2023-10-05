9b712b7da0b9e6fab36fd1074129cd612ca6ac80
Sapir Barzilay <sapirb@elad.co.il>
2023-10-05 19:04:54 +0300

    remove swr

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,7 @@ import Layout from "layout/Layout";
 import theme from "config/theme";
 import { Outlet } from "react-router";
 import GLOBAL_STYLES from "styles/GlobalStyles";
-import { SWRConfig } from "swr";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import SWR_CONFIG from "config/SWR";
 import { GlobalStyles } from "@mui/material";
 import { QueryClientProvider } from "react-query";
 import { QUERY_CLIENT } from "config/reactQuery";
@@ -17,15 +15,13 @@ function App() {
                 <GoogleOAuthProvider
                     clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}
                 >
-                    <SWRConfig value={SWR_CONFIG}>
-                        <QueryClientProvider client={QUERY_CLIENT}>
-                            <GlobalStyles styles={GLOBAL_STYLES} />
+                    <QueryClientProvider client={QUERY_CLIENT}>
+                        <GlobalStyles styles={GLOBAL_STYLES} />
 
-                            <Layout>
-                                <Outlet />
-                            </Layout>
-                        </QueryClientProvider>
-                    </SWRConfig>
+                        <Layout>
+                            <Outlet />
+                        </Layout>
+                    </QueryClientProvider>
                 </GoogleOAuthProvider>
             </ThemeProvider>
         </div>
--- client/src/config/SWR.ts
+++
@@ -1,8 +0,0 @@
-import { SWRConfiguration } from "swr";
-
-const SWR_CONFIG: SWRConfiguration = {
-    errorRetryCount: 3,
-    focusThrottleInterval: 5000,
-};
-
-export default SWR_CONFIG;
