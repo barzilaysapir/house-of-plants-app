3b868d70dd502945eace6ebd3dae62f03f2cb1d7
Sapir Barzilay <sapirb@elad.co.il>
2023-12-23 16:18:34 +0200

    Revert "myplants routing"
    This reverts commit d5c77fb518da2bcceab361533b9f67fd61249aea.

--- client/src/shared/types/route.ts
+++ client/src/shared/types/route.ts
@@ -1,12 +1,6 @@
 export enum Route {
-    HOME = "/",
     SIGN_IN = "/signin",
     MY_PLANTS = "/myPlants",
     REMINDERS = "/reminders",
     IDENTIFY = "/identify",
-}
-
-export enum MyPlantsRoute {
-    PLANTS = "/",
-    SITES = "/sites",
 }
