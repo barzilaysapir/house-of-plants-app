9f6fd6692e8dd8791c51dd32fa4b103ad9a0985b
sapirba <sapirba@checkpoint.com>
2025-01-12 13:31:10 +0200

    upgrade server node

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -3,19 +3,32 @@ import { MongoClient, Db } from "mongodb";
 let _db: Db | null = null;
 
 export const connectToDB = async (callback: (err?: unknown) => void) => {
-    try {
-        const uri = Bun.env.MONGO_URI;
-        const client = new MongoClient(uri!);
-        await client.connect();
-        console.log("Connected to DB");
-        _db = client.db("houseofplants");
-        callback();
-    } catch (err) {
-        callback(err);
-    }
+  try {
+    const uri = Bun.env.MONGO_URI;
+    const client = new MongoClient(uri!);
+    await client.connect();
+    console.log("Connected to DB");
+    _db = client.db("houseofplants");
+    callback();
+  } catch (err) {
+    callback(err);
+  }
 };
 
 export const getDb = (): Db => {
-    if (!_db) throw new Error("DB failed to connect");
-    return _db;
+  if (!_db) throw new Error("DB failed to connect");
+  return _db;
 };
+
+/* 
+    // All API requests should include your API key in an Authorization HTTP header as follows:
+    Authorization: Bearer OPENAI_API_KEY
+
+
+    import OpenAI from "openai";
+
+    const openai = new OpenAI({
+        organization: "org-rFud8zFPXVZskvcznN5Tc2zB",
+        project: "$PROJECT_ID",
+    });
+*/
